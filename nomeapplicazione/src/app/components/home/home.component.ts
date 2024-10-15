27dc1cf3c129c5b25af2a96cb3ec85906414a94f
Andrea-LaVentura75 <laventuraandrea40@gmail.com>
2024-10-15 19:50:44 +0200

    second commit

--- nomeapplicazione/src/app/components/home/home.component.ts
+++ nomeapplicazione/src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Ipost } from '../../interfaces/ipost';
-import { IJSONresponse } from '../../interfaces/i-jsonresponse';
 import { PostService } from '../../services/post.service';
 
 @Component({
@@ -10,30 +9,36 @@ import { PostService } from '../../services/post.service';
 })
 export class HomeComponent {
   constructor(private postSvc: PostService) {}
+
   postCasuale: Ipost[] = [];
   arrayPost!: Ipost[];
+  uniqueTags: string[] = [];
+  postsWithTag!: Ipost[];
+  selectedTag: string | null = null; // Tag selezionato per filtrare
+  filteredPosts: Ipost[] = []; // Post filtrati che saranno visualizzati
+
   ngOnInit() {
     this.postSvc
       .getAllPost()
-      .then((dati: IJSONresponse) => {
+      .then((dati) => {
         this.arrayPost = dati.posts;
-        console.log(this.arrayPost);
-        this.generaPostsCasuali();
+        this.postSvc.setPosts(this.arrayPost); // Imposta i post nel servizio
+        this.filteredPosts = this.arrayPost; // Mostra tutti i post all'inizio
+        this.postCasuale = this.postSvc.generatePostsCasuali(this.arrayPost);
+        this.uniqueTags = this.postSvc.extractUniqueTags(this.arrayPost);
       })
       .catch((error) => {
         console.error('Errore nel recupero dei post:', error);
       });
   }
 
-  generaPostsCasuali() {
-    for (let i: number = 0; i <= 4; i++) {
-      if (this.arrayPost.length > 0) {
-        let numeroCasuale = Math.floor(Math.random() * this.arrayPost.length);
-        this.postCasuale.push(this.arrayPost[numeroCasuale]);
-        console.log(this.postCasuale);
-      } else {
-        console.warn("L'array dei post è vuoto!");
-      }
-    }
+  filterPostsByTag(tag: string): void {
+    this.selectedTag = tag; // Imposta il tag selezionato
+    this.filteredPosts = this.postSvc.filterPostsByTag(tag); // Filtra i post dal service
+  }
+  // Funzione per reimpostare i post (mostra tutti i post)
+  clearFilter(): void {
+    this.selectedTag = null; // Nessun tag selezionato
+    this.filteredPosts = this.arrayPost; // Mostra di nuovo tutti i post
   }
 }
--- nomeapplicazione/src/app/services/post.service.ts
+++ nomeapplicazione/src/app/services/post.service.ts
@@ -1,13 +1,50 @@
 import { Injectable } from '@angular/core';
 import { IJSONresponse } from '../interfaces/i-jsonresponse';
+import { Ipost } from '../interfaces/ipost';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
+  private posts: Ipost[] = [];
+
   constructor() {}
 
-  getAllPost() {
-    return <Promise<IJSONresponse>>fetch('db.json').then((res) => res.json());
+  // Method to get all posts from 'db.json'
+  getAllPost(): Promise<IJSONresponse> {
+    return fetch('db.json').then((res) => res.json()) as Promise<IJSONresponse>;
+  }
+
+  // Method to generate 5 random posts
+  generatePostsCasuali(arrayPost: Ipost[]): Ipost[] {
+    const postsCasuali: Ipost[] = [];
+    for (let i: number = 0; i <= 4; i++) {
+      if (arrayPost.length > 0) {
+        let numeroCasuale = Math.floor(Math.random() * arrayPost.length);
+        postsCasuali.push(arrayPost[numeroCasuale]);
+      }
+    }
+    return postsCasuali;
+  }
+
+  // Method to extract unique tags from posts
+  extractUniqueTags(arrayPost: Ipost[]): string[] {
+    const tagSet = new Set<string>();
+    arrayPost.forEach((post) => {
+      post.tags.forEach((tag) => {
+        tagSet.add(tag);
+      });
+    });
+    return Array.from(tagSet);
+  }
+
+  setPosts(posts: Ipost[]): void {
+    this.posts = posts;
+    console.log(posts);
+  }
+
+  // Method to find posts by a specific tag
+  filterPostsByTag(tag: string): Ipost[] {
+    return this.posts.filter((post) => post.tags.includes(tag)); // Filtra i post
   }
 }
